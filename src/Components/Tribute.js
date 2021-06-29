226be9c6ad87085b40b2dd3dbb468cab4d58becf
tadiwaIV <tadiwachivhere@gmail.com>
2021-06-29 09:11:31 +0200

    starting up tribute page

---
+++ src/Components/Tribute.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Container, Row, Col, Button, Card, Form} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faMobileAlt } from "@fortawesome/free-solid-svg-icons";
+
+const Tribute = () => (
+  <div>
+    <h1> Hello </h1>
+  </div>
+);
+
+export default Tribute;
